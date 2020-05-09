c83a96a912660b0ef8acd25fd9e817d63747697d
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-05-09 14:35:33 -0400

    user interface link added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'admin/index'
   root 'rooms#index'
   devise_for :clients
   resources :rooms
--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class AccueilControllerTest < ActionDispatch::IntegrationTest
+class AdminControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get accueil_index_url
+    get admin_index_url
     assert_response :success
   end
 
