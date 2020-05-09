4e1d9688a7328c3c09b9c2ccc7ccfb83059d2e01
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-05-09 12:35:12 -0400

    RoomsController index-show-new-create and form

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,25 @@
 class RoomsController < ApplicationController  
 
-    def show
-        @room = Room.find(params[:id])
+  def index
+    @rooms = Room.all
+  end
+  
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(params.require(:room).permit(:room_name, :room_description, :room_vue, :room_price, :room_vacancy))
+    if @room.save
+      flash[:notice] = "Room was successfully saved! "
+      redirect_to @room
+    else
+      render 'new'
     end
+  end
 
 end
