0b97c36f9c66d9a49dbec0c352331b424cec21aa
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-05-10 14:53:04 -0400

    update reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,41 @@
 class ReservationsController < ApplicationController
 
-    def show
+  def show
         @reservation = Reservation.find(params[:id])
     end
+
+    def index
+        @reservations = Reservation.all
+    end
+
+    def new
+      @reservation = Reservation.new
+    end
+
+    def edit
+      @reservation = Reservation.find(params[:id])
+    end
+
+
+    def create
+      @reservation = Reservation.new(params.require(:reservation).permit(:date, :room, :client_id, :client_email, :client_request))
+      if @reservation.save
+        flash[:notice] = "La réservation a été enregistrée."
+        redirect_to @reservation
+      else
+        render 'new'
+      end
+    end 
+
+    def update
+      @reservation = Reservation.find(params[:id])
+      if @reservation.update(params.require(:reservation).permit(:date, :room, :client_id, :client_email, :client_request))
+        flash[:notice]= "La réservation a été modifié"
+        redirect_to @reservation
+      else
+        render 'edit'
+      end
+    end
+    
 
 end
